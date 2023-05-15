72de64f8cdee3906ce0df271276df4ce36070442
Niraj Vadher <104006716+NirajVadher07@users.noreply.github.com>
2023-05-15 10:22:47 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,12 @@
+//11th task
+document.querySelector(".wrapper .logo .icon").style.backgroundImage = "url('https://ineuron.ai/images/ineuron-logo.png')"
+
+
+//8th tags
+
+let a = document.querySelectorAll(".z4hgWe a")
+for (let index = 0; index < a.length; index = index+2 ) {
+    
+    a[index].innerHTML = ""
+    
+}
